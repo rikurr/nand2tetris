7e16613d9f765e6bb4e7b2b505979727f9c35ef8
oya riku <56904584+rikurr@users.noreply.github.com>
2023-01-05 10:25:23 +0900

    Add:mult.asm追加

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,35 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+
+@R2 // 計算結果の初期化
+M=0
+
+@count // 掛け算の計算回数
+M=0
+
+(LOOP)
+
+@count
+D=M
+@R1
+D=D-M
+@END
+D;JEQ // 計算回数が到達したらループを抜ける
+
+@R0 // DにR0の値を格納
+D=M
+
+@R2 // R2にM+Dの値を格納
+M=D+M
+
+@count // 計算回数をプラス1
+M=M+1
+
+@LOOP // ループに戻る
+0;JMP
+
+
+(END)
+@END
+0;JMP
