d5df7eab4d7489f3218d1cc0120b4b3d6ce5c7b6
oya riku <56904584+rikurr@users.noreply.github.com>
2023-01-05 12:59:03 +0900

    Add:Fill.asmの処理追加

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,67 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+
+@24575 // スクリーンの最大値のアドレス取得
+D=A
+
+@limit // スクリーンの最大値のアドレスを変数に
+M=D
+
+
+(LOOP) // 無限ループ
+
+@SCREEN // スクリーン開始アドレスを取得
+D=A
+
+@current_screen // 現在のスクリーンアドレスを変数に
+M=D
+
+@KBD // キーボードが押されていたら1
+D=M
+
+@CREAR // スクリーンをクリア
+D;JEQ
+
+(FILL)
+@limit // 最大値のアドレスをDレジスタに保存
+D=M
+
+@current_screen // 現在のスクリーン位置と最大値を比較
+D=M-D
+
+@LOOP // 現在のスクリーン位置が最大値を超えたらENDへ
+D;JGT
+
+// スクリーンを黒くする処理
+@current_screen // 現在のスクリーンを黒くする
+A=M
+AM=-1
+
+@current_screen // 現在のスクリーン位置を移動する
+M=M+1
+
+@FILL
+0;JMP
+
+
+(CREAR)
+@limit // 最大値のアドレスをDレジスタに保存
+D=M
+
+@current_screen // 現在のスクリーン位置と最大値を比較
+D=M-D
+
+@LOOP // 現在のスクリーン位置が最大値を超えたらENDへ
+D;JGT
+
+// スクリーンを白くする処理
+@current_screen // 現在のスクリーンを黒くする
+A=M
+AM=0
+
+@current_screen // 現在のスクリーン位置を移動する
+M=M+1
+
+@CREAR
+0;JMP
